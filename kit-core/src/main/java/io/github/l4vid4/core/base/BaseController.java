dae5de574623be67bfb60991a90598346495abb0
Lang <914551901@qq.com>
2025-05-27 12:15:58 +0800

    v0.1.1

--- kit-core/src/main/java/io/github/l4vid4/core/base/BaseController.java
+++ kit-core/src/main/java/io/github/l4vid4/core/base/BaseController.java
@@ -8,7 +8,6 @@ import io.github.l4vid4.core.model.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
